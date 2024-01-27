30d6c5642b47ea95cb73cccd8598db1ba8ca8314
Mauricio Chaves <100001035+MauricioCh2@users.noreply.github.com>
2024-01-26 23:15:51 -0600

    Add files via upload

---
+++ labBackend/src/ApplicationServer.java
@@ -0,0 +1,9 @@
+import labServer.Server;
+
+public class ApplicationServer {
+    public static void main(String[] args) {
+        Server server = new Server();//instanciamos el server
+        server.run();
+    }
+    //esto deberia de aparecer en el git
+}
